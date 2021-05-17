308ecdce185bef5145ab5ba102e921f2ea361338
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2021-05-17 17:50:34 +0900

    add axios response

--- src/components/pages/Blog.tsx
+++ src/components/pages/Blog.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from 'axios';
+
+import { BlogType } from "../../types/type";
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
 export const Blog:React.FC = () => {
+    const [blogs, setblogs] = useState<Array<BlogType>>();
+    //djangoのurlをenvから取得
+    const onClickFetchData = () => {
+        axios.get<Array<BlogType>>(`http://localhost:8000/api/blog/`, {
+            headers: {
+                "Content-Type": "application/json",
+            },}).then((res) => {
+            console.log(res.data);
+            // setblogs(res.data);
+        });
+    };
     return(
         <SMainContainer>
             <h1>Blog</h1>
             <p>aaaa</p>
             <p>aabbb</p>
+            <button onClick={onClickFetchData}>get</button>
+            <p>{blogs}</p>
         </SMainContainer>
         
     );
---
+++ src/types/type.ts
@@ -0,0 +1,6 @@
+export interface BlogType {
+    id: number;
+    img: File | null;
+    text: string;
+    title: string;
+};
