b055f277938fd105b3beffd596c9ddbbb370bed7
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2021-06-15 01:22:01 +0900

    fix merge

--- src/components/atoms/table/TableHeader.tsx
+++ src/components/atoms/table/TableHeader.tsx
@@ -8,20 +8,10 @@ type Props = {
 };
 
 export const TableHeader:React.FC<Props> = ({children}) => {
-=======
-import styled from "styled-components";
 
-
-export const TableHeader = (props:any) => {
-    const { children } = props;
->>>>>>> 2f129aec9b6f82b4435b3fe5c6ebd36a26a8b762
     return <STh>{children}</STh>;
 };
 
 const STh = styled.th`
-    color: red;
-<<<<<<< HEAD
     border: solid 1px #999;
-=======
->>>>>>> 2f129aec9b6f82b4435b3fe5c6ebd36a26a8b762
 `;
